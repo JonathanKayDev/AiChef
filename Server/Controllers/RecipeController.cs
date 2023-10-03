216b67067957a7cea3bd78c51378a3bfae816519
Jonathan Kay <67706563+JonathanKayDev@users.noreply.github.com>
2023-10-03 20:49:05 +0100

    Add web api controller.

---
+++ Server/Controllers/RecipeController.cs
@@ -0,0 +1,19 @@
+﻿using AiChef.Server.Data;
+using AiChef.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AiChef.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RecipeController : ControllerBase
+    {
+        [HttpPost, Route("GetRecipeIdeas")]
+        public async Task<ActionResult<List<RecipeIdea>>> GetRecipeIdeas(RecipeParams recipeParams)
+        {
+            return SampleData.RecipeIdeas;
+        }
+        
+    }
+}
